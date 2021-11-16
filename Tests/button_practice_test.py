ba33ffb32db81f489c2bc722dab0de8a3201d856
HEHABUSTb <msanvold@gmail.com>
2021-11-16 18:55:45 +0300

    add Logger

--- Tests/button_practice_test.py
+++ Tests/button_practice_test.py
@@ -1,8 +1,11 @@
 import pytest
+
+from Pages.Test_Logger import Logger
 from Pages.base_page import BasePage
 import time
 
 link = 'http://selenium1py.pythonanywhere.com/'
+logger = Logger().getLogger()
 
 
 @pytest.mark.usefixtures('browser')
@@ -10,5 +13,6 @@ class TestClass:
     def test_multi_browser(self):
         page = BasePage(self.browser, link)
         time.sleep(2)
+        logger.info('It should make logfile')
         page.open()
         assert 1 == 2, 'Allure automate screenshot test'
