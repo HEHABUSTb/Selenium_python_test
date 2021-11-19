d3f8f08a99a9b1fbaa2e9df5eb096eefcdd118e9
HEHABUSTb <msanvold@gmail.com>
2021-11-19 21:11:12 +0300

    add allure.step

--- Pages/shop_page.py
+++ Pages/shop_page.py
@@ -1,6 +1,6 @@
+import allure
+
 from Pages.base_page import BasePage
-
-
 from Pages.locators import ShopPageLocators
 
 
@@ -8,6 +8,7 @@ class ShopPage(BasePage):
     link = 'https://rahulshettyacademy.com/angularpractice/shop'
     title = 'ProtoCommerce'
 
+    @allure.step
     def go_to_home_page(self):
         from Pages.home_page import HomePage
         home_button = self.browser.find_element(*ShopPageLocators.home_button)
