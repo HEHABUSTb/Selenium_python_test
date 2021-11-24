7ea1dd502e288b187e8978ccc34d2873f4185519
HEHABUSTb <msanvold@gmail.com>
2021-11-24 21:06:24 +0300

    in def ec_wait increase timeout from 4 to 10

--- Pages/base_page.py
+++ Pages/base_page.py
@@ -41,7 +41,7 @@ class BasePage():
             return False
         return True
 
-    def ec_wait(self, how, what, timeout=4):
+    def ec_wait(self, how, what, timeout=10):
         WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
 
     def is_not_element_present(self, how, what, timeout=4):
